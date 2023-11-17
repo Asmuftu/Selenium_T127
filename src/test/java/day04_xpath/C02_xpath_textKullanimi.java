cde043ba58a1efe97db9d2947458cf1350cc9afe
Ahmed Said Muftu <ahmedmuftu@ahmed.local>
2023-11-17 08:40:52 +0000

    selenium 4. gun

--- src/test/java/day04_xpath/C02_xpath_textKullanimi.java
+++ src/test/java/day04_xpath/C02_xpath_textKullanimi.java
@@ -8,7 +8,12 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.time.Duration;
 
-public class C01_xpath {
+public class C02_xpath_textKullanimi {
+
+     /*
+        Xpath link olmasa bile yazisi olan webelementleri
+        yazi ile locate etmemize imkan tanir
+     */
 
     public static void main(String[] args) throws InterruptedException {
 
@@ -20,21 +25,21 @@ public class C01_xpath {
         //1- https://testotomasyonu.com/addremove/ adresine gidin
         driver.get("https://testotomasyonu.com/addremove/");
         //2- Add Element butonuna basin
-        driver.findElement(By.xpath("//button[@id='sub-btn']")).click();
+        driver.findElement(By.xpath("//button[.='Add']")).click();
         //3- Delete butonu’nun gorunur oldugunu test edin
-        WebElement removeButonu = driver.findElement(By.xpath("//*[@class='remove-btn']"));
+        WebElement removeButonu = driver.findElement(By.xpath("//button[text()='Remove']"));
 
         if (removeButonu.isDisplayed()){
             System.out.println("Remove butonu gorunme testi PASSED");
         }else System.out.println("Remove butonu gorunme testi FAILED");
 
-        //4- Delete tusuna basin
+        //4- Remove tusuna basin
 
-        driver.findElement(By.xpath("//button[@class='remove-btn']")).click();
+        removeButonu.click();
 
         //5- “Add/Remove Elements” yazisinin gorunur oldugunu test edin
 
-        WebElement addRemoveYaziElementi = driver.findElement(By.xpath("//*[@class='container']"));
+        WebElement addRemoveYaziElementi = driver.findElement(By.xpath("//*[contains(text(),'Remove Elements')]"));
 
         if (addRemoveYaziElementi.isDisplayed()){
             System.out.println("Add Remove Yazisi gorunme testi PASSED");
@@ -43,7 +48,7 @@ public class C01_xpath {
         // Remove butonunun gorunur olmadigini test edin
 
         try {
-            removeButonu = driver.findElement(By.xpath("//*[@class='remove-btn']"));
+            removeButonu = driver.findElement(By.xpath("//button[text()='Remove']"));
             System.out.println("Remove butonu gorunmeme testi FAILED");
         } catch (NoSuchElementException e) {
             System.out.println("Remove butonu gorunmeme testi PASSED");
@@ -52,4 +57,5 @@ public class C01_xpath {
         driver.quit();
 
     }
+
 }
